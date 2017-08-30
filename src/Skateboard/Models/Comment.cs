a483e4afcb00136448fb855dcbc47242b40f1db3
davidrolfs <davidrolfs15@gmail.com>
2017-08-30 15:11:46 -0700

    add comment class

---
+++ src/Skateboard/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Skateboard.Models
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Remark { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
