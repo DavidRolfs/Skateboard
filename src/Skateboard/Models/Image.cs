0a247a6cf11e17ec09e2a49565aadb940eb03a43
davidrolfs <davidrolfs15@gmail.com>
2017-08-30 14:34:13 -0700

    add images model

---
+++ src/Skateboard/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Skateboard.Models
+{
+    [Table("Images")]
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
